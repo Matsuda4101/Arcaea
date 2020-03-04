d95ca746158f36b4af686b7520f1facd61ebbb71
T.Mazda <inukakinekokaki@gmail.com>
2020-03-04 16:40:32 +0900

    完成 我は満足や・・・

---
+++ Assets/スクリプト/選曲画面/All_Select.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class All_Select : MonoBehaviour
+{
+    public GameObject[] song;
+    static GameObject[] song_200 = new GameObject[201];
+    int number;
+    int count = 0;
+    int wait = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = Random.Range(0, 151);
+        song_200[count / 100] = Instantiate(song[number], new Vector3(0, 0, -1), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = count + 25;
+
+        if (count % 100 == 0 && count <= 5000)
+        {
+            Destroy(song_200[(count / 100) - 1]);
+
+            number = Random.Range(0, 151);
+            song_200[count / 100] = Instantiate(song[number], new Vector3(0, 0, -1), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/スクリプト/選曲画面/Crimsom_Select.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crimsom_Select : MonoBehaviour
+{
+    public GameObject[] song;
+    static GameObject[] song_46 = new GameObject[101];
+    int number;
+    int count = 0;
+    int wait = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = Random.Range(0, 6);
+        song_46[count / 100] = Instantiate(song[number], new Vector3(0, 0, -1), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = count + 25;
+
+        if (count % 100 == 0 && count <= 5000)
+        {
+            Destroy(song_46[(count / 100) - 1]);
+
+            number = Random.Range(0, 6);
+            song_46[count / 100] = Instantiate(song[number], new Vector3(0, 0, -1), Quaternion.identity);
+        }
+    }
+}
